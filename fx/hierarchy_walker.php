52463cd12ec621feed992cbe3607d6a404dd4480
Mark Parnell <markparnell@brownbox.net.au>
2016-08-31 08:33:15 +1000

    Added hierarchy walker function

---
+++ fx/hierarchy_walker.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Works its way up the post hierarchy to find a meta value
+ * @param string $field Meta key to look for
+ * @param integer $id (optional) Post ID to start from
+ * @param integer $last_id (optional) Variable to hold the ID of the post the value was found on (or the top-level ancestor if no value found)
+ * @return mixed
+ */
+function get_value_from_hierarchy($field, $id = null, &$last_id = null, $type = "meta") {
+    if (empty($id)) {
+        global $post;
+        if (empty($post))
+            return ''; // No global $post var available.
+        $id = $post->ID;
+    }
+
+    $ancestors = get_ancestors($id, get_post_type($id));
+    do {
+        $last_id = $id;
+        if ($type == 'taxonomy') {
+            $value = wp_get_object_terms($id, $field);
+        } else {
+            $value = get_post_meta($id, $field, true);
+        }
+        $id = array_shift($ancestors);
+    } while ($id > 0 && empty($value));
+
+    return $value;
+}
