03d14abc2617a689c9d0ec4b0430c92167fa08ff
Mark Parnell <markparnell@brownbox.net.au>
2016-10-18 15:43:07 +1100

    Code cleanup

--- 404.php
+++ 404.php
@@ -1,3 +1,4 @@
-<?php get_header(); ?>
-<?php bb_theme::section('name=content&file=404.php&inner_class=row'); ?>
-<?php get_footer(); ?>
+<?php
+get_header();
+bb_theme::section('name=content&file=404.php&inner_class=row');
+get_footer();
