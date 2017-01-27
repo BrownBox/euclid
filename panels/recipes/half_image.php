f78df4742ea5f71e64f668136b24b4d9dd89b326
Mark Parnell <markparnell@brownbox.net.au>
2017-01-27 16:27:34 +1100

    Reworked layout of half image panel slightly

--- panels/recipes/half_image.php
+++ panels/recipes/half_image.php
@@ -4,19 +4,21 @@ if (!empty($meta["image"][0])) {
     $image = wp_get_attachment_image_src($meta["image"][0], 'full');
 }
 ?>
-<div class="row image-<?php echo $meta["image_pos"][0]; ?>">
-    <div class="column">
-        <div class="content">
+<div class="image-<?php echo $meta["image_pos"][0]; ?>">
+	<div class="image" style="background-image: url(<?php echo $image[0]; ?>); background-size:cover;"></div>
+	<div class="row">
+	    <div class="column">
+	        <div class="content">
 <?php
 bb_panel_title($panel);
 bb_panel_content($panel);
 if (!empty($meta["destination"][0])) {
 ?>
-            <p class="action-button"><a href="<?php echo $meta["destination"][0]; ?>" class="button"><?php echo $meta["action_text"][0]; ?></a></p>
+	            <p class="action-button"><a href="<?php echo $meta["destination"][0]; ?>" class="button button-border font-weight-600"><?php echo $meta["action_text"][0]; ?></a></p>
 <?php
 }
 ?>
-        </div>
-    </div>
+	        </div>
+	    </div>
+	</div>
 </div>
-<div class="image" style="background-image: url(<?php echo $image[0]; ?>); background-size:cover;"></div>
