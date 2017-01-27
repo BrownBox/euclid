f330b7713e9a76197ea0f63f7b6c7d7c1f650ca4
Mark Parnell <markparnell@brownbox.net.au>
2017-01-27 15:57:56 +1100

    Updated hero logic to make better use of hierarchy

--- fx/hero.php
+++ fx/hero.php
@@ -1,10 +1,11 @@
 <?php
 /**
  * Gets the hero image URL for large, medium and small screens
- * @param string $post
+ * @param mixed $post (optional) Post ID or WP_Post object. If empty will use global $post.
+ * @param integer $last_id (optional) Variable to hold the ID of the post the large image was found on (or the top-level ancestor if no value found)
  * @return boolean|array False on error, else associative array of screen sizes => URLs
  */
-function bb_get_hero_images($post = null) {
+function bb_get_hero_images($post = null, &$last_id = null) {
     if (is_null($post)) {
         global $post;
     } else {
@@ -14,21 +15,23 @@ function bb_get_hero_images($post = null) {
         return false;
     }
 
-    $meta = get_post_meta($post->ID);
-    if (!empty($meta['hero_image'][0])) {
-        $large_image = wp_get_attachment_image_src($meta["hero_image"][0], 'full');
+    $large_hero_meta = get_value_from_hierarchy('hero_image', $post->ID, $last_id);
+    if (!empty($large_hero_meta)) {
+        $large_image = wp_get_attachment_image_src($large_hero_meta, 'full');
         $large_hero = $large_image[0];
     } else {
-        $large_hero = get_value_from_hierarchy('featured_image', $post->ID);
+        $large_hero = get_value_from_hierarchy('featured_image', $post->ID, $last_id);
     }
-    if (!empty($meta['hero_image_medium'][0])) {
-        $medium_image = wp_get_attachment_image_src($meta['hero_image_medium'][0], 'full');
+    $medium_hero_meta = get_value_from_hierarchy('hero_image_medium', $post->ID);
+    if (!empty($medium_hero_meta)) {
+        $medium_image = wp_get_attachment_image_src($medium_hero_meta, 'full');
         $medium_hero = $medium_image[0];
     } else {
         $medium_hero = $large_hero;
     }
-    if (!empty($meta['hero_image_small'][0])) {
-        $small_image = wp_get_attachment_image_src($meta['hero_image_small'][0], 'full');
+    $small_hero_meta = get_value_from_hierarchy('hero_image_small', $post->ID);
+    if (!empty($small_hero_meta)) {
+        $small_image = wp_get_attachment_image_src($small_hero_meta, 'full');
         $small_hero = $small_image[0];
     } else {
         $small_hero = $large_hero;
