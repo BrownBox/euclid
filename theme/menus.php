beab45d545eb2ec825e6424896593bfdc1b45e1d
Mark Parnell <markparnell@brownbox.net.au>
2017-01-30 11:54:33 +1100

    Fix bug with menu item CSS classes

--- theme/menus.php
+++ theme/menus.php
@@ -43,22 +43,22 @@ function bb_menu($args, $echo = true) {
                 foreach ((array)$menu_items as $key => $menu_item) {
                     // Get direct children menu items
                     $direct_children = get_direct_children($menu_item, $menu_items);
-                    $parent_class = '';
                     $ancestors = get_ancestors($post->ID, 'page');
+                    $this_li_class = $li_class;
 
-                    if ($display_children && !empty($direct_children)) {
-                        $parent_class = 'has-children';
-                        $li_class = in_array($menu_item->object_id, $ancestors) || $post->ID == $menu_item->object_id || (is_home() && get_option('page_for_posts') == $menu_item->object_id) ? 'active uncollapsed' : '';
-                    } else {
-                        $li_class = in_array($menu_item->object_id, $ancestors) || $post->ID == $menu_item->object_id || (is_home() && get_option('page_for_posts') == $menu_item->object_id) ? 'active' : '';
+                    if (in_array($menu_item->object_id, $ancestors) || $post->ID == $menu_item->object_id || (is_home() && get_option('page_for_posts') == $menu_item->object_id) || get_the_slug($menu_item->object_id) == $post->post_type) {
+                        $this_li_class .= ' active';
+                        if ($display_children && !empty($direct_children)) {
+                            $this_li_class .= ' uncollapsed has-children';
+                        }
                     }
 
                     if ($menu_item->menu_item_parent == 0) {
-                        $ourmenu .= '<li class="' . $li_class . ' menu-item menu-item-' . $menu_item->ID . ' ' . $menu_item->classes[0] . '">';
+                        $ourmenu .= '<li class="'.$this_li_class.' menu-item menu-item-'.$menu_item->ID.' '.$menu_item->classes[0].'">';
                         if (strpos($menu_item->url, $modal) > 0) {
-                            $ourmenu .= '<a data-reveal-id="' . $modal . '_' . $menu_item->object_id . '" href="#">' . $menu_item->title . '</a>';
+                            $ourmenu .= '<a data-reveal-id="'.$modal.'_'.$menu_item->object_id.'" href="#">'.$menu_item->title.'</a>';
                         } else {
-                            $ourmenu .= '<a class="' . $class . '" href="' . $menu_item->url . '">' . $menu_item->title . '</a>';
+                            $ourmenu .= '<a class="'.$class.'" href="'.$menu_item->url.'">'.$menu_item->title.'</a>';
                         }
 
                         // Add child menu items if they exist
@@ -66,13 +66,16 @@ function bb_menu($args, $echo = true) {
                             $ourmenu .= '<ul class="sub-menu">';
                             foreach ($direct_children as $direct_child) {
                                 // Build classes for sub menu item container
-                                $li_class_child = in_array($direct_child->object_id, $ancestors) || $post->ID == $direct_child->object_id || (is_home() && get_option('page_for_posts') == $direct_child->object_id) ? 'active' : '';
-                                $ourmenu .= '<li class="' . $li_class_child . ' menu-item menu-item-' . $direct_child->ID . ' ' . $direct_child->classes[0] . '">';
+                                $li_class_child = $li_class;
+                                if (in_array($direct_child->object_id, $ancestors) || $post->ID == $direct_child->object_id || (is_home() && get_option('page_for_posts') == $direct_child->object_id) || get_the_slug($direct_child->object_id) == $post->post_type) {
+                                    $li_class_child .= ' active';
+                                }
+                                $ourmenu .= '<li class="'.$li_class_child.' menu-item menu-item-'.$direct_child->ID.' '.$direct_child->classes[0].'">';
 
                                 if (strpos( $direct_child->url, $modal ) > 0) {
-                                    $ourmenu .= '<a data-reveal-id="' . $modal . '_' . $direct_child->object_id . '" href="#">' . $direct_child->title . '</a>';
+                                    $ourmenu .= '<a data-reveal-id="'.$modal.'_'.$direct_child->object_id.'" href="#">'.$direct_child->title.'</a>';
                                 } else {
-                                    $ourmenu .= '<a class="' . $class . '" href="' . $direct_child->url . '">' . $direct_child->title . '</a>';
+                                    $ourmenu .= '<a class="'.$class.'" href="'.$direct_child->url.'">'.$direct_child->title.'</a>';
                                 }
                                 $ourmenu .= '</li>';
                             }
