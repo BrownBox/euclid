6b048b70320528bb84ab243621d7e8623120c2c7
Mark Parnell <markparnell@brownbox.net.au>
2017-02-02 11:28:58 +1100

    Added transient to bb_get_children()

--- fx/children.php
+++ fx/children.php
@@ -11,13 +11,27 @@ function bb_get_children($post = null) {
     if (is_int($post)) {
         $post = get_post($post);
     }
-    $args = array(
-            'posts_per_page' => -1,
-            'orderby' => array('menu_order' => 'ASC', 'title' => 'ASC'),
-            'post_type' => get_post_type($post),
-            'post_parent' => $post->ID,
-    );
-    return get_posts($args);
+
+    $transients = defined(WP_BB_ENV) && WP_BB_ENV == 'PRODUCTION'; // Set this to false to force all transients to refresh
+    $transient = ns_.'children_'.$post->ID.'_';
+    if (false === $transients) {
+        delete_transient($transient);
+    }
+    if (false === ($children = get_transient($transient))) {
+        $args = array(
+                'posts_per_page' => -1,
+                'orderby' => array('menu_order' => 'ASC', 'title' => 'ASC'),
+                'post_type' => get_post_type($post),
+                'post_parent' => $post->ID,
+        );
+        $children = get_posts($args);
+
+        if (true === $transients) {
+            set_transient($transient, $children, LONG_TERM);
+        }
+    }
+
+    return $children;
 }
 /**
  * Check if the post has children
