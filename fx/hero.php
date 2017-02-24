eb9820d33352770c3c6431dd865978f4decc6f2e
Mark Parnell <markparnell@brownbox.net.au>
2017-02-24 15:35:12 +1100

    Store hero images in transient

--- fx/hero.php
+++ fx/hero.php
@@ -15,31 +15,44 @@ function bb_get_hero_images($post = null, &$last_id = null) {
         return false;
     }
 
-    $large_hero_meta = get_value_from_hierarchy('hero_image', $post->ID, $last_id);
-    if (!empty($large_hero_meta)) {
-        $large_image = wp_get_attachment_image_src($large_hero_meta, 'full');
-        $large_hero = $large_image[0];
-    } else {
-        $large_hero = get_value_from_hierarchy('featured_image', $post->ID, $last_id);
+    $transients = defined(WP_BB_ENV) && WP_BB_ENV == 'PRODUCTION'; // Set this to false to force all transients to refresh
+    $transient = ns_.'hero_'.$post->ID.'_';
+    if (false === $transients) {
+        delete_transient($transient);
     }
-    $medium_hero_meta = get_value_from_hierarchy('hero_image_medium', $post->ID);
-    if (!empty($medium_hero_meta)) {
-        $medium_image = wp_get_attachment_image_src($medium_hero_meta, 'full');
-        $medium_hero = $medium_image[0];
-    } else {
-        $medium_hero = $large_hero;
-    }
-    $small_hero_meta = get_value_from_hierarchy('hero_image_small', $post->ID);
-    if (!empty($small_hero_meta)) {
-        $small_image = wp_get_attachment_image_src($small_hero_meta, 'full');
-        $small_hero = $small_image[0];
-    } else {
-        $small_hero = $large_hero;
-    }
+    if (false === ($images = get_transient($transient))) {
+        $large_hero_meta = get_value_from_hierarchy('hero_image', $post->ID, $last_id);
+        if (!empty($large_hero_meta)) {
+            $large_image = wp_get_attachment_image_src($large_hero_meta, 'full');
+            $large_hero = $large_image[0];
+        } else {
+            $large_hero = get_value_from_hierarchy('featured_image', $post->ID, $last_id);
+        }
+        $medium_hero_meta = get_value_from_hierarchy('hero_image_medium', $post->ID);
+        if (!empty($medium_hero_meta)) {
+            $medium_image = wp_get_attachment_image_src($medium_hero_meta, 'full');
+            $medium_hero = $medium_image[0];
+        } else {
+            $medium_hero = $large_hero;
+        }
+        $small_hero_meta = get_value_from_hierarchy('hero_image_small', $post->ID);
+        if (!empty($small_hero_meta)) {
+            $small_image = wp_get_attachment_image_src($small_hero_meta, 'full');
+            $small_hero = $small_image[0];
+        } else {
+            $small_hero = $large_hero;
+        }
 
-    return array(
-            'large' => $large_hero,
-            'medium' => $medium_hero,
-            'small' => $small_hero,
-    );
+        $images = array(
+                'large' => $large_hero,
+                'medium' => $medium_hero,
+                'small' => $small_hero,
+        );
+        if (true === $transients) {
+            set_transient($transient, $images, LONG_TERM);
+        }
+    }
+    unset($transient);
+
+    return $images;
 }
