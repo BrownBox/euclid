3783d284d1f29eca1b86fb9d6912fcaa1291397e
Mark Parnell <markparnell@brownbox.net.au>
2017-03-02 12:08:02 +1100

    Better logic for hero images and other hierarchy-driven values

--- fx/hero.php
+++ fx/hero.php
@@ -6,51 +6,53 @@
  * @return boolean|array False on error, else associative array of screen sizes => URLs
  */
 function bb_get_hero_images($post = null, &$last_id = null) {
-    if (is_null($post)) {
-        global $post;
-    } else {
-        $post = get_post($post);
-    }
-    if (!($post instanceof WP_Post)) {
-        return false;
-    }
+    $post = get_post($post);
 
     $transients = defined(WP_BB_ENV) && WP_BB_ENV == 'PRODUCTION'; // Set this to false to force all transients to refresh
     $transient = ns_.'hero_'.$post->ID.'_';
     if (false === $transients) {
         delete_transient($transient);
     }
-    if (false === ($images = get_transient($transient))) {
+
+    if ($post instanceof WP_Post) {
         $large_hero_meta = get_value_from_hierarchy('hero_image', $post->ID, $last_id);
         if (!empty($large_hero_meta)) {
             $large_image = wp_get_attachment_image_src($large_hero_meta, 'full');
             $large_hero = $large_image[0];
-        } else {
-            $large_hero = get_value_from_hierarchy('featured_image', $post->ID, $last_id);
         }
+    }
+    if (empty($large_hero)) {
+        $large_hero = get_value_from_hierarchy('featured_image', $post->ID, $last_id);
+    }
+
+    if ($post instanceof WP_Post) {
         $medium_hero_meta = get_value_from_hierarchy('hero_image_medium', $post->ID);
         if (!empty($medium_hero_meta)) {
             $medium_image = wp_get_attachment_image_src($medium_hero_meta, 'full');
             $medium_hero = $medium_image[0];
-        } else {
-            $medium_hero = $large_hero;
         }
+    }
+    if (empty($medium_hero)) {
+        $medium_hero = $large_hero;
+    }
+    if ($post instanceof WP_Post) {
         $small_hero_meta = get_value_from_hierarchy('hero_image_small', $post->ID);
         if (!empty($small_hero_meta)) {
             $small_image = wp_get_attachment_image_src($small_hero_meta, 'full');
             $small_hero = $small_image[0];
-        } else {
-            $small_hero = $large_hero;
         }
+    }
+    if (empty($small_hero)) {
+        $small_hero = $large_hero;
+    }
 
-        $images = array(
-                'large' => $large_hero,
-                'medium' => $medium_hero,
-                'small' => $small_hero,
-        );
-        if (true === $transients) {
-            set_transient($transient, $images, LONG_TERM);
-        }
+    $images = array(
+                    'large' => $large_hero,
+                    'medium' => $medium_hero,
+                    'small' => $small_hero,
+    );
+    if (true === $transients) {
+        set_transient($transient, $images, LONG_TERM);
     }
     unset($transient);
 
--- fx/hierarchy_walker.php
+++ fx/hierarchy_walker.php
@@ -2,33 +2,31 @@
 /**
  * Works its way up the post hierarchy to find a meta value
  * @param string $field Key to look for
- * @param integer $id (optional) Post ID to start from
+ * @param integer|WP_Post $post (optional) Post to start from
  * @param integer $last_id (optional) Variable to hold the ID of the post the value was found on (or the top-level ancestor if no value found)
  * @param string $type Type of data to look for. Accepts either "meta" (default) or "taxonomy".
  * @return mixed
  */
-function get_value_from_hierarchy($field, $id = null, &$last_id = null, $type = "meta") {
-    if (empty($id)) {
-        global $post;
-        if (empty($post))
-            return ''; // No global $post var available.
-        $id = $post->ID;
-    }
+function get_value_from_hierarchy($field, $post = null, &$last_id = null, $type = "meta") {
+    $post = get_post($post);
 
-    $ancestors = get_ancestors($id, get_post_type($id));
-    do {
-        $last_id = $id;
-        if ($type == 'taxonomy') {
-            $value = wp_get_object_terms($id, $field);
-        } else {
-            if ($field == 'featured_image') {
-                $value = bb_get_featured_image_url('full', $id);
+    if ($post instanceof WP_Post) {
+        $id = $post->ID;
+        $ancestors = get_ancestors($id, get_post_type($id));
+        do {
+            $last_id = $id;
+            if ($type == 'taxonomy') {
+                $value = wp_get_object_terms($id, $field);
             } else {
-                $value = get_post_meta($id, $field, true);
+                if ($field == 'featured_image') {
+                    $value = bb_get_featured_image_url('full', $id);
+                } else {
+                    $value = get_post_meta($id, $field, true);
+                }
             }
-        }
-        $id = array_shift($ancestors);
-    } while ($id > 0 && empty($value));
+            $id = array_shift($ancestors);
+        } while ($id > 0 && empty($value));
+    }
 
     // Check for fallback default from customizer
     if (empty($value)) {
